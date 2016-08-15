d1c361c479c115bc0e8cc2fba002188787018d43
Vikas Gautam <v33vikasgautam@gmail.com>
2016-08-14 19:50:21 -0700

    Finalised the API

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
    var session = require("request").defaults({jar: true})
    // For parsing html, not a memory leaker like cheerio
    var whacko = require("whacko")
+   // URL parser
    var URL = require("url")
 
    // Method for making sync http request to simplyfy the api
@@ -13,26 +14,7 @@
          "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36"
       }
       session.get(options, function (err, resp) {
-         if (resp.statusCode === 302) {
-            var pathName = URL.parse(resp.headers.location, true)
-            // Checking if google redirected to captcha page
-            if (pathName !== "/search") {
-               var captchaPage = get({
-                  uri: resp.headers.location,
-                  followRedirect: false
-               })
-               captchaPage.resp.statusCode = 302
-               cb(null, captchaPage)
-            // First google search url will always redirect, so
-            // following the redirection manually.
-            } else {
-               var redirectedResp = get({
-                  uri: resp.headers.location,
-                  followRedirect: false
-               })
-               cb(null, redirectedResp)
-            }
-         } else if ((err || resp.statusCode !== 200)) {
+         if ((err || resp.statusCode !== 200) && resp.statusCode !== 503) {
             cb(null, {
                err: resp.statusCode + " - " + resp.statusText
             })
@@ -65,18 +47,17 @@
       // Requesting page
       var firstResp = get({
          uri: self.host + "/search",
-         qs: self.params,
-         followRedirect: false
+         qs: self.params
       })
 
-      // 302 means captcha case
-      if (firstResp.resp.statusCode === 302) {
-         console.log("Captcha Case")
+      // 503 means captcha case
+      if (firstResp.resp.statusCode === 503 && 
+          firstResp.resp.request.uri.href.search("/sorry")) {
          var solvedResp = self.solveCaptcha(firstResp)
          cb(null, solvedResp)
          return
       }
-      cb(null, firstResp)      
+      cb(null, firstResp)
    }
 
    // Sync function
@@ -86,11 +67,11 @@
       var self = this
 
       var $ = googleCaptchaResp.parsed
-      var captchaId = $('input[name=id]').attr('value');
+      var q = $('input[name=q]').attr('value')
+      var captchaId = $('input[name=id]').attr('value')
       var continueUrl = $('input[name=continue]').attr('value')
       var formAction = $('form').attr('action')
-      var imgURL = $('img').attr('src')
-      console.log("captchaId :-",captchaId, "\ncontinueUrl:-", continueUrl, "\nformAction:-", formAction, "\nimgURL:-", imgURL)
+      var imgURL = URL.resolve("https://ipv4.google.com", $('img').attr('src'))
 
       // Getting captcha image
       var imgResp = get({
@@ -100,15 +81,14 @@
       if (imgResp.err) { return imgResp }
 
       // Solving captcha
-      var captchaResp = getCaptchaSolved(imgResp.resp.body)
+      var captchaResp = self.getCaptchaSolvedSync(imgResp.resp.body)
       if (captchaResp.err) { return captchaResp }
 
       // Trying solution
-      //https://www.google.co.in/search?q=suresh%20chauhan%20linkedin&google_abuse=GOOGLE_ABUSE_EXEMPTION%3DID%3D2e4a020e39dddc18:TM%3D1470951076:C%3Dc:IP%3D122.172.176.63-:S%3DAPGng0tdGEfgxDpgo3THOg21AQu6fdvXBA%3B+path%3D/%3B+domain%3Dgoogle.com%3B+expires%3DFri,+12-Aug-2016+00:31:16+GMT
-
       var secondResp = get({
          uri: self.host + "/sorry/" + formAction,
          qs: {
+            q: q,
             id: captchaId,
             captcha: captchaResp.solution,
             continue: continueUrl
@@ -118,7 +98,8 @@
       return secondResp
    }
 
-   gsearch.prototype.getCaptchaSolved = deasync(function (img, cb) {
+   gsearch.prototype.getCaptchaSolved = function (img, cb) {
+      var self = this
       self.options.solver.solve(img, function (err, id, solution) {
          if (err) {
             cb(null, {
@@ -132,7 +113,9 @@
             })
          }
       })
-   })
+   }
+
+   gsearch.prototype.getCaptchaSolvedSync = deasync(gsearch.prototype.getCaptchaSolved)
 
    module.exports = gsearch
 
